089a9bce7891337ad7c9348e04cf11a6906632f6
josawa <josawa@themeetgroup.com>
2024-09-25 09:32:32 -0300

    Pazaak card

--- src/components/GameCard/Backface/BackfaceStandard.tsx
+++ src/components/GameCard/Backface/BackfaceStandard.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import styles from "./Backface.module.css";
+import standardStyles from "./BackfaceStandard.module.css";
+
+const HalfCard: React.FC<{ className?: string }> = ({ className = "" }) => {
+  return (
+    <div className={`${standardStyles.halfContainer} ${className}`}>
+      <span className={standardStyles.cornerContainer}>
+        <span className={standardStyles.cornerContent}>
+          <h4>..</h4>
+        </span>
+      </span>
+
+      <span
+        className={`${standardStyles.trapezoid} ${standardStyles.trapezContainer}`}
+      >
+        <span
+          className={`${standardStyles.trapezoid} ${standardStyles.bgShape} ${standardStyles.outerShape}`}
+        >
+          <span
+            className={`${standardStyles.trapezoid} ${standardStyles.innerTrapezoid}`}
+          />
+        </span>
+      </span>
+    </div>
+  );
+};
 
 export const BackfaceStandard: React.FC = () => {
   return (
-    <div className={`${styles.backface} ${styles.standard}`}>
-      Back side
+    <div className={`${styles.backface} ${standardStyles.base}`}>
+      <div className={standardStyles.artContainer}>
+        <HalfCard />
+
+        <HalfCard className={styles.inverted} />
+      </div>
     </div>
   );
-}
+};
