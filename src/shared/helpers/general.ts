23dd5d3b20e0477cc18d87ae53126657bcd0d2d8
josawa <josawa@themeetgroup.com>
2024-09-26 11:06:51 -0300

    More comments

--- src/shared/helpers/general.ts
+++ src/shared/helpers/general.ts
@@ -12,6 +12,8 @@ export const withNamespace = (baseTxt: string): string => {
 export function arrayShuffle<T>(baseArray: T[]): T[] {
   for (let i = baseArray.length - 1; i > 0; i--) {
     const targetIndex = Math.floor(Math.random() * (i + 1));
+
+    // At this line I reassign the array item value
     [baseArray[i], baseArray[targetIndex]] = [baseArray[targetIndex], baseArray[i]];
   }
 
